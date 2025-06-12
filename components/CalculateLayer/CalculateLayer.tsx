35f6ec5cc60a4e39bd0c1564e7517cac258f8012
ranokhassanova <khasanova.rano@gmail.com>
2025-06-12 18:59:45 +0500

    add: hero

---
+++ components/CalculateLayer/CalculateLayer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CalculateLayer = () => {
+  return (
+    <div className='bg-white'>
+        <h2>Рассчитайте стоимость уборки</h2>
+    </div>
+  )
+}
+
+export default CalculateLayer
